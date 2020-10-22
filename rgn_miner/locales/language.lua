b8c909985ef5d352ba4cfaa184caa3e555a4a96f
Braden <57928428+Kyronomix@users.noreply.github.com>
2020-10-22 12:07:43 -0400

    Update language.lua for ES and FR configuration.

--- rgn_miner/locales/language.lua
+++ rgn_miner/locales/language.lua
@@ -10,14 +10,15 @@ Language.translate = {
         ['goto'] = 'Dirígete a la siguiente localización',
         ['load'] = 'Carga todos los minerales en el vehículo que te hemos dejado fuera',
         ['carry'] = 'Lleva todos los minerales al cliente',
-        ['completejob'] = 'Has completado tu trabajo, has ganado ',
+        ['completejob'] = 'Ha completado su trabajo, por cada acero entregado que ha ganado ',
         ['noveh'] = 'Tienes que Deliveryr todos los minerales junto al vehículo proporcionado',
         ['temp'] = 'TEMPORIZADOR: ',
         ['seconds'] = ' SEGUNDOS RESTANTES',
         ['lose'] = 'No has llegado a tiempo, trabajo cancelado',
         ['pressf'] = 'Pulsa [ENTER] para Deliveryr los minerales',
         ['mining'] = 'Trabajando en la mina',
-        ['placing'] = 'Colocando la bolsa'
+        ['placing'] = 'Colocando la bolsa',
+	['cantcarrymore'] = 'No puedes llevar más de ese artículo'
     },
     ['en'] = {
         ['press'] = 'Press [ENTER] to start work',
@@ -36,7 +37,7 @@ Language.translate = {
         ['pressf'] = 'Press [ENTER] to deliver the minerals',
         ['mining'] = 'Working in the mine',
         ['placing'] = 'Placing the bag',
-		['cantcarrymore'] = 'You cannot carry more of that item'
+	['cantcarrymore'] = 'You cannot carry more of that item'
     },
     ['fr'] = {
         ['press'] = 'Appuyer sur [ENTER] pour commencer a travailler',
@@ -47,13 +48,14 @@ Language.translate = {
         ['goto'] = 'Diriger vous vers le prochain marqueur',
         ['load'] = 'Chargez tous les minérais dans le véhicule que nous avons laissé',
         ['carry'] = 'Apporter tous les minérais au client',
-        ['completejob'] = 'Vous avez terminé votre travail, vous avez gagné ',
+        ['completejob'] = 'Vous avez terminé votre travail, pour chaque acier livré que vous avez gagné ',
         ['noveh'] = 'Vous devez livrer tous les minérais dans le véhicule fourni',
         ['temp'] = 'TEMPS: ',
         ['seconds'] = ' SECONDES RESTANTES',
         ['lose'] = 'Vous nêtes pas arrivé à lheure, le travail a été annulé',
         ['pressf'] = 'Appuyer sur [ENTER] pour délivrer les minéraux',
         ['mining'] = 'Travailler dans la mine',
-        ['placing'] = 'Mise en place du sac'
+        ['placing'] = 'Mise en place du sac',
+	['cantcarrymore'] = 'Vous ne pouvez pas transporter plus de cet article'
     }
 }
